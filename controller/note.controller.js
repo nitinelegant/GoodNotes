d1cae4026654bfb26c8766c015e4ea8a999aab0a
NitinSinghElegant <130339696+NitinSinghElegant@users.noreply.github.com>
2024-11-01 19:58:21 +0530

    add note api

--- controller/note.controller.js
+++ controller/note.controller.js
@@ -28,3 +28,97 @@ export const addNote = async (req, res, next) => {
     next(error);
   }
 };
+export const editNote = async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params.noteId);
+    if (!note) {
+      return next(errorHandler(404, "Note not found"));
+    }
+    if (note.userId !== req.user.id) {
+      return next(errorHandler(403, "You can only edit your own notes"));
+    }
+    const { title, content, tags, isPinned } = req.body;
+    if (!title && !tags && !content && !isPinned) {
+      return next(errorHandler(400, "No fields to update"));
+    }
+    if (title) {
+      note.title = title;
+    }
+    if (content) {
+      note.content = content;
+    }
+    if (tags) {
+      note.tags;
+    }
+    if (isPinned) {
+      note.isPinned = isPinned;
+    }
+    await note.save();
+
+    res.status(201).json({
+      succuess: true,
+      message: "Note updated successfully",
+      note,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllNotes = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const notes = await Note.find({ userId }).sort({ isPinned: -1 });
+
+    res.status(201).json({
+      succuess: true,
+      message: "All notes fetched successfully",
+      notes,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const deleteNote = async (req, res, next) => {
+  try {
+    const noteId = req.params.noteId;
+    const note = await Note.findOne({ _id: noteId, userId: req.user.id });
+    if (!note) {
+      return next(errorHandler(404, "Note not found"));
+    }
+    if (note.userId !== req.user.id) {
+      return next(errorHandler(403, "You can only delete your own notes"));
+    }
+    await note.deleteOne({ _id: noteId });
+
+    res.status(201).json({
+      succuess: true,
+      message: "Note deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const updatePinnedNote = async (req, res, next) => {
+  try {
+    const noteId = req.params.noteId;
+    const note = await Note.findById({ _id: noteId });
+    if (!note) {
+      return next(errorHandler(404, "Note not found"));
+    }
+    if (note.userId !== req.user.id) {
+      return next(errorHandler(403, "You can only delete your own notes"));
+    }
+    const { isPinned } = req.body;
+    note.isPinned = isPinned;
+    await note.save();
+
+    res.status(201).json({
+      succuess: true,
+      message: "Note updated successfully",
+      note,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/note.route.js
+++ routes/note.route.js
@@ -1,9 +1,19 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyToken.js";
-import { addNote } from "../controller/note.controller.js";
+import {
+  addNote,
+  editNote,
+  getAllNotes,
+  deleteNote,
+  updatePinnedNote,
+} from "../controller/note.controller.js";
 
 const router = express.Router();
 
 router.post("/add", verifyToken, addNote);
+router.post("/edit/:noteId", verifyToken, editNote);
+router.get("/all", verifyToken, getAllNotes);
+router.delete("/delete/:noteId", verifyToken, deleteNote);
+router.put("/update-pinned-note/:noteId", verifyToken, updatePinnedNote);
 
 export default router;
